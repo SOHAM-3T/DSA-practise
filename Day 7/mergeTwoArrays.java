c4c729e7d431320d2811ab9ebb15a80392696de6
Soham <soham4net@gmail.com>
2025-09-14 22:50:05 +0530

    Solved Merging two sorted arrays

---
+++ Day 7/mergeTwoArrays.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution {
+    public void mergeArrays(int a[], int b[]) {
+        // code here
+        int i = a.length - 1;
+        int j = 0;
+        while (i >= 0 && j < b.length) {
+            if (a[i] < b[j])
+                i--;
+            else {
+                int temp = b[j];
+                b[j] = a[i];
+                a[i] = temp;
+                i--;
+                j++;
+            }
+        }
+        // Sort both arrays
+        Arrays.sort(a);
+        Arrays.sort(b);
+    }
+}
