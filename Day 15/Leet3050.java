1bc74f06a5cc07ee215743f59adeb2ab78ee2909
Soham <soham4net@gmail.com>
2025-09-22 23:53:57 +0530

    Solved Leet 3050

---
+++ Day 15/Leet3050.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int maxFreq = 0;
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            maxFreq = Math.max(maxFreq, freq.get(num));
+        }
+        int total = 0;
+        for (int f : freq.values()) {
+            if (f == maxFreq) {
+                total += f;
+            }
+        }
+
+        return total;
+    }
+}
