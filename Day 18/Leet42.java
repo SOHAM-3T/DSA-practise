8125f953d403ad2ba0fe3cde38ef74618e2fc323
Soham <soham4net@gmail.com>
2025-09-25 20:05:49 +0530

    Solved Leet 42

---
+++ Day 18/Leet42.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height){
+        int n = height.length;
+        int[] left = new int[n]; 
+        int[] right = new int[n];
+        if(n == 0){
+            return 0;
+        }
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            max = Math.max(max,height[i]);
+            left[i] = max;
+        }
+        max = 0;
+        for(int i = n-1; i>=0; i--){
+            max = Math.max(max,height[i]);
+            right[i] = max;
+        }
+        max = 0;
+        for(int i = 0; i<n; i++){
+            max += Math.min(left[i],right[i]) - height[i];
+        }
+
+        return max;
+    }
+}
