a699a53c04bbd3e734c3d2509130e0d62c7b0624
Soham <soham4net@gmail.com>
2025-10-02 19:32:46 +0530

    Solved Leet 3100

--- Day 25/Leet3100.java
+++ Day 25/Leet3100.java
@@ -1,3 +1,12 @@
-public class Leet3100 {
-    
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int sum = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            empty -= numExchange-1;
+            sum += 1;  
+            numExchange++;     
+        }
+        return sum;
+    }
 }
