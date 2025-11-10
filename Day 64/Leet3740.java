922037737a1fe14d9b5dce90ed9f1ed919a66110
Soham <soham4net@gmail.com>
2025-11-11 00:52:37 +0530

    Solved Leet3740

--- Day 64/Leet3740.java
+++ Day 64/Leet3740.java
@@ -1,29 +1,21 @@
-import java.util.*;
 class Solution {
     public int minimumDistance(int[] nums) {
-        Map<Integer, Integer> lastSeen = new HashMap<>();
-        Map<Integer, Integer> secondLastSeen = new HashMap<>();
-
-        int minDistance = Integer.MAX_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            int value = nums[i];
-
-            if (secondLastSeen.containsKey(value)) {
-                int firstIndex = secondLastSeen.get(value);
-                int currentDist = 2 * (i - firstIndex);
-                minDistance = Math.min(minDistance, currentDist);
-                secondLastSeen.put(value, lastSeen.get(value));
-                lastSeen.put(value, i);
-                
-            } else if (lastSeen.containsKey(value)) {
-                secondLastSeen.put(value, lastSeen.get(value));
-                lastSeen.put(value, i);
-                
-            } else {
-                lastSeen.put(value, i);
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]==nums[j] && nums[j]==nums[k]){
+                        int dist = Math.abs(i-j) + Math.abs(j-k) + Math.abs(k-i);
+                        ans = Math.min(ans,dist);
+                    }
+                }
             }
         }
-        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+        if(ans==Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return ans;
+        }
     }
 }
