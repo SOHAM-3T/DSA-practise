b608b9ba3e16494dd3d1da4ee8c68389a6dd732f
Soham <soham4net@gmail.com>
2025-11-24 23:25:53 +0530

    Solved Leet 1018

---
+++ Day 78/Leet1018.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+  public List<Boolean> prefixesDivBy5(int[] nums) {
+    List<Boolean> ans = new ArrayList<>();
+    int curr = 0;
+
+    for (int num : nums) {
+      curr = (curr * 2 + num) % 5;
+      ans.add(curr % 5 == 0);
+    }
+
+    return ans;
+  }
+}
